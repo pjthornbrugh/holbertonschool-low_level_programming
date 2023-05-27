be64596ac75f5879430b5ddd00867f4cc3f94cc9
Pilar Thornbrugh <pilarthornbrugh@Pilars-MBP.lan>
2023-05-27 13:27:00 -0500

    4-puts.c 2nd push :

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,3 @@
 #!/bin/bash
-/**
-* main - Entry point
-*
-*Return: Always 0 (Success)
-*/
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle
-");
-	return (0);
-}
+#include<stdio.h>
+int main(void) { puts ("Programming is like building a multilingual puzzle\n"); return (0); } 
