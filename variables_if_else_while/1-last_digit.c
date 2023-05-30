6750e6ca0ce4b12149a1ae1d292ace2e74309882
Pilar Thornbrugh <pilarthornbrugh@Pilars-MacBook-Pro.local>
2023-05-30 14:29:43 -0500

    1-last again

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -16,15 +16,15 @@ int main(void)
 
 	if ((n % 10) > 5)
         {
-                printf("Last digit of %d is greater than 5\n", n);
+                printf("Last digit of %d and is greater than 5\n"), n;
         }
         else if ((n % 10) < 6 && (n % 10) != 0)
         {
-                printf("Last digit of %d is less than 6 and not 0\n", n);
+                printf("Last digit of %d and is less than 6 and not 0\n"), n;
         }
         else
         {
-                printf("Last digit of %d is %d and is 0\n", n);
+                printf("Last digit of %d is %d and is 0\n"), n;
         }
         return (0);
 }
