10eec753bf56ea84d9a4f9c5da5f6f802a0bdd60
Pilar Thornbrugh <pilarthornbrugh@Pilars-MacBook-Pro.local>
2023-05-30 14:37:37 -0500

    1-last chance

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
         }
         else
         {
-                printf("Last digit of %d is %d and is 0\n", n);
+                printf("Last digit of %d is 0 and is 0\n", n);
         }
         return (0);
 }
