e817f07f429a6ab931745fb9286a4e6237eb3558
Pilar Thornbrugh <pilarthornbrugh@Pilars-MacBook-Pro.local>
2023-05-30 11:01:11 -0500

    char

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 /**
-* main - Entrt point
+* main - Entry point
 *
 * return: Always 0 (Success)
 */
 int main(void)
 {
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char);
+	printf("Size of an int: %zu byte(s)\n", sizeof(char);
+	printf("Size of a long int: %zu byte(s)\n", sizeof(char);
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(char);
+	printf("Size of a float: %zu byte(s)\n", sizeof(char);
 	return (0);
 }
