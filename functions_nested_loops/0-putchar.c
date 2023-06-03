9b815994ae8c2045ac73fef2ec9f4a992d52b671
Pilar Thornbrugh <pilarthornbrugh@Pilars-MBP.lan>
2023-06-03 15:55:57 -0500

    0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,32 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/*
+ * File: holberton.h
+ * Auth: Pilar J Thornbrugh
+ * Desc: Header file containing declarations for all functions
+ *       used in the 0x02-functions_nested_loops directory.
+ */
+
+#include "holberton.h"
+
+/**
+ * main - This prints _putchar followed by a new line.
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+
+}
