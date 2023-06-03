ccaa7f8fc8b3946e4d3bb42bf11346cbb287c154
Pilar Thornbrugh <pilarthornbrugh@Pilars-MBP.lan>
2023-06-03 15:22:51 -0500

    6-prints_numbers.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+/*
+ * File: 6-prints_numbers.c
+ * Auth: Pilar J Thornbrugh
+ */
+
+#include <stdio.h>
+
+/**
+ * main- prints all single digit numbers of base 10 starting form 0,
+ * 	 only using putchar and without char variables.
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
