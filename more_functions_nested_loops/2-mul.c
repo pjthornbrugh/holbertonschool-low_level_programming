f363c3e7dc3e20c953e52432cb833fb19acd693c
Pilar Thornbrugh <pilarthornbrugh@Pilars-MBP.lan>
2023-06-03 13:32:26 -0500

    2-mul.c push

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+/*
+ * File: 2-mul.c
+ * Auth: Pilar J Thornbrugh
+ */
+
+#include "main.h"
+
+/**
+ * mul- Multiples two intergers.
+ * @a: the first interger to be multiplied.
+ * @b: the second interger to be muliplied.
+ *
+ * Return: the result of a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
