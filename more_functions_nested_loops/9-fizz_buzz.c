60292db4b24fb3369b6c5534eaebe868cdeeb9a9
Pilar Thornbrugh <pilarthornbrugh@Pilars-MBP.lan>
2023-06-03 14:40:51 -0500

    9-fizz_buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -18,7 +18,7 @@ int main(void)
 
 	for (num = 1; num <=100; num++)
 	{
-		if ((num % 3) == 0 && (num % 5) ==0))
+		if ((num % 3) == 0 && (num % 5) ==0)
 			printf("FizzBuzz");
 
 		else if ((num % 3) == 0)
