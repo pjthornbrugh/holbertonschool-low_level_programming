a642af1422dced372fdbe769a0043f38cfb6165f
Pilar Thornbrugh <pilarthornbrugh@Pilars-MBP.lan>
2023-06-03 15:04:59 -0500

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+/*
+ * File: 2-print_alphabet.c
+ * Auth: Pilar J Thornbrugh
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints the alaphabet in lowercase.
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; lerrer <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+
+	return (0);
+
+}
