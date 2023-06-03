e5d1b6a4b3c6194145370ed82fb1dc58160359b3
Pilar Thornbrugh <pilarthornbrugh@Pilars-MBP.lan>
2023-06-03 14:37:21 -0500

    9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+/*
+ * File: 9=fizz_buzz.c
+ * Auth: Pilar J Thornbrugh
+ */
+
+#include "main.c"
+
+/**
+ * main - prints the nums 1-100, but for mult of 3, fizz is printed instead of 3
+ *	  for mult of 5, buzz is printed
+ *	  for mult of both 3 and 5, fizzbuzz is printed
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <=100; num++)
+	{
+		if ((num % 3) == 0 && (num % 5) ==0))
+			printf("FizzBuzz");
+
+		else if ((num % 3) == 0)
+			printf("Fizz");
+
+		else if ((num % 5) == 0)
+			printf("Buzz");
+
+		else
+			printf("%d", num);
+
+		if ( num == 100)
+			continue;
+		printf(" ");
+	}
+
+	printf("\n");
+
+	return (0);
+}
