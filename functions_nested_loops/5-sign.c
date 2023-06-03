93f9f026602b1db08732bddbdc34c435e5d92c8b
Pilar Thornbrugh <pilarthornbrugh@Pilars-MBP.lan>
2023-06-03 16:37:15 -0500

    5-sign.c

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+/*
+ * File: 5-sign.c
+ * Auth: Pilar J Thornbrugh
+ */
+
+#include "main.h"
+
+/*
+ * print_sign - prints the sign of a number.
+ * @n: the number of which the sign will print.
+ *
+ * Return: 1 if the num is > than 0,
+ *	   0 if the num is 0,
+ *	   -1 id the num is less than 0.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
