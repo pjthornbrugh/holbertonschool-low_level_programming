bac605455b258d71f714fd74e77c5a5fe6d1e79b
Pilar Thornbrugh <pilarthornbrugh@Pilars-MBP.lan>
2023-06-03 14:58:18 -0500

    1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -14,18 +14,19 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	x = n % 10
-	if ( x > 5 )
+	if (( n % 10) > 5 )
 	{
- 		printf("Last digit of %d is %s and is greater than 5\n", n);
+ 		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	}
-	else if ((x % 10) < 6 && (x % 10) != 0)
+	else if ((n % 10) < 6 && (n % 10) != 0)
 	{
-		printf("Last digit of %d is %s and is less than 6 and not 0\n", n);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	}
 	else
 	{
-		printf("Last digit of %d is 0 and is 0\n", n);
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	}
+
 	return (0);
+
 }
