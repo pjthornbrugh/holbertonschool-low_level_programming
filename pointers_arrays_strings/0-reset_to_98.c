80c081def20dd9990940b2f6be6e0d0abdd8993b
Pilar Thornbrugh <pilarthornbrugh@Pilars-MBP.lan>
2023-06-04 13:48:35 -0500

    0-reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+/*
+ * File: 0-reser_to_98.c
+ * Auth: Pilar J Thornbrugh
+ */
+
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int and updates the value.
+ *
+ * @n: the pointer to an int.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
