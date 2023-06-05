71c194988b51d533f2d930b7ceeca66a16220a64
Pilar Thornbrugh <pilarthornbrugh@Pilars-MacBook-Pro.local>
2023-06-05 13:19:32 -0500

    8-print_array.c

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+/*
+ * File: 8-print_arrau.c
+ * Auth: Pilar J Thornbrugh
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array of integers. new line.
+ * @a: array.
+ * @n: number of elements.
+ *
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < (n - 1); i++)
+		printf("%d, ", a[i]);
+	if (i != 0)
+		printf("%d", a[i]);
+	printf("\n");
+}
