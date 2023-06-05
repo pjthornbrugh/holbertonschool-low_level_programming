c5a10aac940343caa8087c472417075a71d20979
Pilar Thornbrugh <pilarthornbrugh@Pilars-MacBook-Pro.local>
2023-06-05 12:16:56 -0500

    3-puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -10,11 +10,9 @@
  * @str: the string to print.
  */
 void _puts(char *str)
-{
-	while (*str != '\0' ) 
-	{
-		_putchar(*str);
-		str++;
-	}
+}
+	while (*str) 
+		_putchar(*str++);
+
 	_putchar('\n');
 }
