03c0203947e35796ee8817df680fceee25eeb682
Pilar Thornbrugh <pilarthornbrugh@Pilars-MacBook-Pro.local>
2023-06-05 13:26:16 -0500

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+/*
+ * File: 10-add.c
+ * Auth: Pilar J Thornbrugh 
+ */
+
+#inlcude "main.h"
+
+/**
+ * add -  adds two integers and returns the result.
+ * @a: first int.
+ * @b: second int.
+ *
+ * Return: a+b.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
