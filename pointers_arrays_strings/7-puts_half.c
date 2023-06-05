d3260e1811032263181095090156c28bf91e9923
Pilar Thornbrugh <pilarthornbrugh@Pilars-MacBook-Pro.local>
2023-06-05 13:16:35 -0500

    7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+/*
+ * File: 7-puts_half.c
+ * Auth: Pilar J Thornbrugh
+ */
+
+#include "holberton.h"
+#include "2-strlen.c"
+
+/**
+ * puts_half - prints half of a string, followed by a new line.
+ * @str: string.
+ *
+ */
+void puts_half(char *str)
+{
+	int half, length = _strlen(str);
+	int i;
+
+	if (length % 2 == 0)
+		half = length / 2;
+	else
+		half = (length - 1) / 2;
+
+	i = length - half;
+	while (i < length)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
