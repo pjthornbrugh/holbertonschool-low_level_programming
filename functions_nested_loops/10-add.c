51fb20a2f1228d37f5990f6495b82400a83be810
Pilar Thornbrugh <pilarthornbrugh@Pilars-MacBook-Pro.local>
2023-06-05 13:29:37 -0500

    11-print_to_98.c

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,6 +1,6 @@
 /*
  * File: 10-add.c
- * Auth: Pilar J Thornbrugh 
+ * Auth: Pilar J Thornbrugh
  */
 
 #include "main.h"
---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+/*
+ * File: 11-print_to_98.c
+ * Auth: Pilar J Thornbrugh
+ */
+
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints all natural numbers from n to 98, followed by new line.
+ * @n: from
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i < 98; i++)
+		printf("%i, ", i);
+	for (i = n; i > 98; i--)
+		printf("%i, ", i);
+	printf("98\n");
+}
