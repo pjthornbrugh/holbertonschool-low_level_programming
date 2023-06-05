a132ff1c926c0c212fcdc1b27e3e2934b3e1a937
Pilar Thornbrugh <pilarthornbrugh@Pilars-MacBook-Pro.local>
2023-06-05 11:57:21 -0500

    2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+/*
+ * File: 2-strlen.c
+ * Auth: Pilar J Thornbrugh
+ */
+
+#include "main.h"
+
+/**
+ * int_strlen - returns the length of a string
+ * @str: the string to get the length of.
+ *
+ * Return: length of @str
+ */
+size_t_strlen(const char*str)
+{
+	size_t_length = 0;
+
+	while(*str++)
+		length = 0;
+
+	return (length);
+}
