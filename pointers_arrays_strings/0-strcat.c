4c9461177ee81be1bbeaf6421463b1df0b7bb112
Pilar Thornbrugh <pilarthornbrugh@Pilars-MBP.lan>
2023-06-12 11:39:39 -0500

    0-strcat.c

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+/*
+ * File: 0-strcat.c
+ * Auth: Pilar J Thornbrugh
+ */
+
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings together.
+ * *dest: destination
+ * *src: source
+ *
+ * Return: pointer to the resulting string destination.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j = 0;
+	char* ptr = dest;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+
+	for (; src[j] != '\0'; j++)
+	{
+		dest[j] = src[j];
+	}
+	dest[i] = '\0';
+
+	return ptr;
+}
