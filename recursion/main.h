142cde5929545dc1c499f829f95307f79c518b6e
Pilar Thornbrugh <pilarthornbrugh@Pilars-MBP.lan>
2023-06-17 12:28:24 -0500

    6-is_prime_number.c

--- recursion/main.h
+++ recursion/main.h
@@ -19,7 +19,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-int prime_function(int n, int y);
+int prime_function(int n, int i);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 
