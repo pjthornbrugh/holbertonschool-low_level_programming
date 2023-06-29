a4ee7ba2787831e8701e1d7390b741fd0eae12ef
Pilar Thornbrugh <pilarthornbrugh@Pilars-MBP.lan>
2023-06-29 11:06:02 -0500

    add

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -16,8 +16,8 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] !=0; j++)
-			if (argv[i][j] < 48 || argv [i][j] > 57)
+		for (j = 0; argv[i][j] != 0; j++)
+			if (argv[i][j] < 48 || argv[i][j] > 57)
 			{
 				printf("Error\n");
 				return (1);
