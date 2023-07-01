290fdb58fb67325b81b523fa879245c2bda8bb6e
Pilar Thornbrugh <pilarthornbrugh@Pilars-MacBook-Pro.local>
2023-07-01 11:49:06 -0500

    string_nconcat

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - creates an array of integers,
+ * with values from min (included) to max (included).
+ * @min: start value.
+ * @max: final value.
+ * Return: pointer to the array.
+ **/
+int *array_range(int min, int max)
+{
+	int i, length, *arr;
+
+	if (min > max)
+		return (NULL);
+	length = max - min + 1;
+
+	arr = malloc(sizeof(int) * length);
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; (i + min) <= max; i++)
+		arr[i] = i + min;
+	return (arr);
+}
