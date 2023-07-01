64aa5ec48ebfd7d49a1029101acfeca122741310
Pilar Thornbrugh <pilarthornbrugh@Pilars-MacBook-Pro.local>
2023-07-01 11:12:19 -0500

    init_dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -8,7 +8,8 @@
  *
  */
 
-/** struct dog - a new type describing a dog.
+/**
+ * struct dog - a new type describing a dog.
  * @name: the name of the dog.
  * @age: the age of the dog.
  * @owner: the owner of the dog.
