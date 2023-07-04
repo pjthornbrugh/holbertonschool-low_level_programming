2206b78e46f01971d43b735193c05a3d104e4d6b
Pilar Thornbrugh <pilarthornbrugh@Pilars-MBP.lan>
2023-07-04 14:05:48 -0500

    3-function_like_macro.h

---
+++ preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef ABS
+#define ABS
+
+/*
+ * main - writes a function like macro that computes the absolute value of a num.
+ *
+ * Return: absolute value of num.
+ **/
+
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+#endif
