f1e758d29fc7cc57d700f03c361645da64179570
Pilar Thornbrugh <pilarthornbrugh@Pilars-MBP.lan>
2023-07-16 14:48:11 -0500

    code for task 1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+/**
+ * size_t - returns the number of elements in a linked list
+ * @h - the pointer
+ * @list_t - linked list in question
+ *
+ * Return: the number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
