c8352670fee1ef07dda60f986093bee3279c498c
Pilar Thornbrugh <pilarthornbrugh@Pilars-MBP.lan>
2023-07-16 15:22:13 -0500

    code for 2-add_node.c

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -4,21 +4,6 @@
 #include <string.h>
 
 /**
- * struct list_s - linked list structure
- * @str: string
- * @len: length of the string
- * @next: points to next node
- *
- */
-typedef struct list_s
-{
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t; 
-
-
-/**
  * add_node - adds nodes to the beginning of a linked list.
  * @head: head of the linked list.
  * @str: string to be duplicated and added to the linked list.
@@ -28,23 +13,25 @@ typedef struct list_s
 
 list_t *add_node(list_t **head, const char *str)
 {
-	if (str == NULL)
-		return NULL;
+	list_t *new_node;
 
-	list_t *new_node = malloc(sizeof(list_t));
+	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
+	{
 		return NULL;
+	}
 
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
 		free(new_node);
-		return NULL;
+		return (NULL);
 	}
 
-	new_node->len = strlen(str);
+	new_node->len = strlen(new_node->str);
 	new_node->next = *head;
 
 	*head = new_node;
-	return new_node;
+
+	return (new_node);
 }
