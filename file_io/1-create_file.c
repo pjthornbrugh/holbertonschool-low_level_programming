279a6182808f5ecf0c2a9431eb6aed27d62ea18a
Pilar Thornbrugh <pilarthornbrugh@Pilars-MacBook-Pro.local>
2023-07-17 15:26:27 -0500

    fixing betty errors

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -25,7 +25,8 @@ int create_file(const char *filename, char *text_content)
 		return (1);
 	}
 
-	for (len = 0; text_content[len]; len++);
+	for (len = 0; text_content[len]; len++)
+	;
 
 	wr_stat = write(new_file, text_content, len);
 
