3a4a0fd11f4aad02436259de6612581fed392b60
Pilar Thornbrugh <pilarthornbrugh@Pilars-MacBook-Pro.local>
2023-07-17 15:11:28 -0500

    README and file creation

---
+++ file_io/main.h
@@ -0,0 +1,16 @@
+#ifndef FILE_IO
+#define FILE_IO
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#define BUFSIZE 1204
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int _putchar(char c);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
