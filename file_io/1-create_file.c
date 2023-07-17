192e852a71c8726a5377e39a93c25530e488da5a
Pilar Thornbrugh <pilarthornbrugh@Pilars-MacBook-Pro.local>
2023-07-17 15:17:41 -0500

    checking 1-create_file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of the file
+ * @text_content: content of file, if NULL create an empty file.
+ *
+ * Return: if filename is null, return -1, 1 if a success
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int new_file, len, wr_stat;
+
+	if (filename == NULL)
+		return (-1);
+
+	new_file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	if (new_file == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		close(new_file);
+		return (1);
+	}
+
+	for (len = 0; text_content[len]; len++);
+
+	wr_stat = write(new_file, text_content, len);
+
+	if (close(new_file) == -1)
+		return (-1);
+
+	return (wr_stat == -1 ? -1 : 1);
+}
