6a8c93868820a86552798c7a3d493fd4ffdd8250
Pilar Thornbrugh <pilarthornbrugh@Pilars-MacBook-Pro.local>
2023-07-17 15:57:11 -0500

    fixing too many chars

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -9,7 +9,7 @@
  * Return: 0 on success
  */
 
-int main(int argcount, char *arrarg[])
+int main(int argcount, char *aarg[])
 {
 	int fd_from, fd_to, rd_stat, wr_stat;
 	mode_t perm = S_IRUSR | S_IWUSR | S_IWGRP | S_IRGRP | S_IROTH;
@@ -17,14 +17,14 @@ int main(int argcount, char *arrarg[])
 
 	if (argcount != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	fd_from = open(arrarg[1], O_RDONLY);
+	fd_from = open(aarg[1], O_RDONLY);
 
 	if (fd_from == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", arrarg[1]), exit(98);
-	fd_to = open(arrarg[2], O_CREAT | O_WRONLY | O_TRUNC, perm);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", aarg[1]), exit(98);
+	fd_to = open(aarg[2], O_CREAT | O_WRONLY | O_TRUNC, perm);
 
 	if (fd_to == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", arrarg[2]), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", aarg[2]), exit(99);
 	rd_stat = 1;
 
 	while (rd_stat)
@@ -32,14 +32,14 @@ int main(int argcount, char *arrarg[])
 		rd_stat = read(fd_from, buffer, BUFSIZE);
 
 		if (rd_stat == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", arrarg[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", aarg[1]), exit(98);
 
 		if (rd_stat > 0)
 		{
 			wr_stat = write(fd_to, buffer, rd_stat);
 
 			if (wr_stat != rd_stat || wr_stat == -1)
-				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", arrarg[1]), exit(99);	
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", aarg[2]), exit(99);
 		}
 	}
 
