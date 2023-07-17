2b064fb79fa7bc504c7a8b76363b0c8c984a4449
Pilar Thornbrugh <pilarthornbrugh@Pilars-MacBook-Pro.local>
2023-07-17 13:30:15 -0500

    code for 1-print_binary

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_binary - prints the binary representation of a num.
+ * @n: unisgned long int
+ *
+ * Return: the binary representation of said num.
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary (n >> 1);
+
+	_putchar((n & 1) + '0'
+}
