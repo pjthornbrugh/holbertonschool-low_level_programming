16af6519d9c3732e45ffd001cde85af536ed4f04
Pilar Thornbrugh <pilarthornbrugh@Pilars-MacBook-Pro.local>
2023-07-17 14:12:53 -0500

    flip_bits

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * flip_bit - returns the num of bits you would need to flip
+ * @n: base num
+ * @m: num to trasform to
+ * Return: the num of bits transformation needed
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int i, res;
+
+	res = 0;
+	for (i = 8 * sizeof(n) - i; i >= 0; i--)
+		if (((n ^ m) >> i) & 1)
+			res++;
+	return (res);
+}
