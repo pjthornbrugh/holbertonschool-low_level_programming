0c2be453bfcedcbeec06443bff4c073d70056500
Pilar Thornbrugh <pilarthornbrugh@Pilars-MacBook-Pro.local>
2023-07-17 13:47:00 -0500

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-print_binary.c -o b issues

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * set_bit - sets the value of a bit to 1
+ * @n: number in question
+ * @index: index
+ *
+ * Return: 1 if it worked, -1 if an error occurred
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int aux = 1 << index;
+
+	if (index > (sizeof((*n) * 8))
+		return (-1);
+
+	*n = *n | aux;
+	return (1);
+}
