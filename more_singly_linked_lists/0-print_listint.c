d61fc31d74b635de88d069811f797cb4dc408957
Pilar Thornbrugh <pilarthornbrugh@Pilars-MacBook-Pro.local>
2023-07-17 17:00:28 -0500

    fixing gcc crap

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_listint - prints all the elements of a list_t list
+ * print_listint - prints all the elements of a listt list
  * @h: singly linked list to print
  * @list_t: list
  * Return: num of nodes in list_t
