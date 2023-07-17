df706af63a29433805729609623ad63c6de2432d
Pilar Thornbrugh <pilarthornbrugh@Pilars-MacBook-Pro.local>
2023-07-17 13:49:18 -0500

    fixing gcc issues, hopefully

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -13,7 +13,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int aux = 1 << index;
 
-	if (index > (sizeof((*n) * 8))
+	if (index > (sizeof((*n) * 8)))
 		return (-1);
 
 	*n = *n | aux;
