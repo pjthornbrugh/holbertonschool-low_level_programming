8d2474675426cfc14d892067319fd226e018eb12
Pilar Thornbrugh <pilarthornbrugh@Pilars-MBP.lan>
2023-07-24 11:18:26 -0500

    just testing a push

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -10,7 +10,7 @@
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_T *new, *aux = *head;
+	dlistint_t *new, *aux = *head;
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
