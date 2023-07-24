e5c2d3a23f1906b0bc84fd0e7be2c76426344775
Pilar Thornbrugh <pilarthornbrugh@Pilars-MBP.lan>
2023-07-24 10:55:52 -0500

    code for returning the num of elements

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * dlistnint_len - returns the num of elements in a linked list
+ * @h: pointer to list.
+ * Return: number of nodes.
+ **/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *node = h;
+	size_t cont = 0;
+
+	while (node)
+	{
+		cont++;
+		node = node->next;
+	}
+
+	return (cont);
+}	
